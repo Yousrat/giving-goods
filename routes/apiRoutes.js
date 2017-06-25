f9e104c86096af4e1972e2129a7202816673cd9c
radhika sivarajan <radhikaraj333@gmail.com>
2017-06-25 15:14:05 -0700

    Api routes

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,6 +1,8 @@
 var express = require("express");
 var router = new express.Router();
 var userController = require("../controllers/userController");
+var peopleController = require("../controllers/peopleController");
+var itemController = require("../controllers/itemController");
 
 // Get all users
 router.get("/user-list", userController.allUsers);
@@ -10,5 +12,19 @@ router.post("/new-user", userController.addUser);
 router.get("/get-my-info", userController.getMyInfo);
 // Update logged in user info
 router.post("/update-my-info", userController.updateMyInfo);
+
+// Get all people
+router.get("/all-people-list", peopleController.allPeople);
+// Get all people to specific shelter
+router.get("/my-people-list", peopleController.myPeople);
+// Add new people
+router.post("/new-people", peopleController.addPeople);
+
+// Get all item
+router.get("/all-item-list", itemController.allItem);
+// Get all item to specific people
+router.get("/my-item-list", itemController.myItem);
+// Add new item
+router.post("/new-item", itemController.addItem);
 
 module.exports = router;
