50dbfaeb0ad8359ad5e3e3ee1a000f8312e23ad9
Yousrat <ybanna@gmail.com>
2017-06-27 13:04:43 -0700

    updates

--- app/components/home/Banner.js
+++ app/components/home/Banner.js
@@ -3,7 +3,7 @@ var Banner = React.createClass({
     render: function () {
         return (
             <div className="row" id="banner">
-			<img src="assets/images/image1.jpg" alt="giving-goods" className="img-responsive center-block" /> 
+			<img src="assets/images/image1.jpg" alt="giving-goods" className="img-responsive center-block main-banner" /> 
             </div>
         );
     }
