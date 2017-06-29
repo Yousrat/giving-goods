57f09ead4a1fb5c26e6e1a9b82754a4e410545f4
Yousrat <ybanna@gmail.com>
2017-06-29 10:27:06 -0700

    contactUs.js

--- app/components/home/ContactUs.js
+++ app/components/home/ContactUs.js
@@ -1,9 +1,50 @@
 var React = require("react");
+var helpers = require("./../../utils/helper");
+var NotificationSystem = require('react-notification-system');
+
 var ContactUs = React.createClass({
+
+    handleChange: function (event) {
+        var newState = {};
+        newState[event.target.id] = event.target.value;
+        this.setState(newState);
+    },
+
+    handleSubmit: function (event) {
+        event.preventDefault();
+        helpers.default.contactAdmin({
+            clientEmail: this.state.clientEmail,
+            clientSubject: this.state.clientSubject,
+            clientMessage: this.state.clientMessage
+        }).then((mailStatus) => {
+            console.log(mailStatus);
+        });
+    },
+
     render: function () {
         return (
             <div className="row" id="contact-us">
-                Contact us
+                <div className="col-md-6 col-md-offset-3">
+                    <div className=" panel panel-default">
+                        <div className="panel-body">
+                            <form onSubmit={this.handleSubmit}>
+                                <div className="form-group">
+                                    <label htmlFor="clientEmail">Email</label>
+                                    <input type="text" placeholder="Enter your email address" className="form-control" id="clientEmail" onChange={this.handleChange} required />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="clientSubject">Subject</label>
+                                    <input type="text" placeholder="Enter Subject" className="form-control" id="clientSubject" onChange={this.handleChange} required />
+                                </div>
+                                <div className="form-group">
+                                    <label htmlFor="clientMessage">Message</label>
+                                    <textarea placeholder="Write your message here" className="form-control" id="clientMessage" onChange={this.handleChange} rows = "5" required />
+                                </div>
+                                <button type="submit" className="btn btn-primary">Send</button>
+                            </form>
+                        </div>
+                    </div>
+                </div>
             </div>
         );
     }
