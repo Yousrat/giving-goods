8a91c7cb93247a7397b0d862b924b88ad41e746a
mightyminh <minhtuyenmai@gmail.com>
2017-06-29 11:37:08 -0700

    Model and controller files update.

---
+++ models/item.js
@@ -0,0 +1,25 @@
+var mongoose = require("mongoose");
+
+var Schema = mongoose.Schema;
+
+var itemSchema = new Schema({
+    people_id: {
+        type: Schema.Types.ObjectId,
+        ref: 'People'
+    },
+    item_name: {
+        type: String,
+        required: true
+    },
+    quantity: {
+        type: String
+    },
+    item_status: {
+        default: 0
+    },
+    description: String
+});
+
+var Item = mongoose.model("Item", itemSchema);
+
+module.exports = Item;
