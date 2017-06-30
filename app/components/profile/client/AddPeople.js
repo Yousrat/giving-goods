2ce3a7df43f634812b08cd2ebf8658456ea44de3
mightyminh <minhtuyenmai@gmail.com>
2017-06-30 10:09:53 -0700

    people client folder

---
+++ app/components/profile/client/AddPeople.js
@@ -0,0 +1,95 @@
+var React = require("react");
+var NotificationSystem = require('react-notification-system');
+var helper = require("./../../../utils/helper");
+
+var AddPeople = React.createClass({
+    _notificationSystem: null,
+    componentDidMount: function () {
+        this._notificationSystem = this.refs.notificationSystem;
+    },
+    handleChange: function (event) {
+        let newState = {};
+        newState[event.target.name] = event.target.value;
+        this.setState(newState);
+    },
+    handleSubmit: function (event) {
+        event.preventDefault();
+        var newUserInfo ={
+            firstName: this.state.newPeopleFirstName,
+            lastName: this.state.newPeopleLastName,
+            peopleCode: this.state.newPeopleCode,
+            ageGroup: this.state.newPeopleAgeGroup,
+            gender: this.state.newPeopleGender,
+            peopleNotes: this.state.newPeopleNotes
+        };
+        helper.default.addNewPeople(newUserInfo).then((newUser) => {
+            if (newUser) {
+                this._notificationSystem.addNotification({
+                    message: 'New user added',
+                    level: 'success',
+                    position: 'tr'
+                });
+            } else {
+                this._notificationSystem.addNotification({
+                    message: 'Error',
+                    level: 'error',
+                    position: 'tr'
+                });
+            }
+        });
+    },
+    render: function () {
+        return (
+            <div id="add-people" className="tab-pane fade">
+                <NotificationSystem ref="notificationSystem" />
+                Add People
+                <div className="row">
+                     <div className="col-md-6 col-md-offset-3">
+                        <form onSubmit={this.handleSubmit}>
+                            <div className="form-group">
+                                <label>First name</label>
+                                <input type="text" className="form-control" name="newPeopleFirstName" onChange={this.handleChange} required />
+                            </div>
+                            <div className="form-group">
+                                <label >Last name</label>
+                                <input type="text" className="form-control" name="newPeopleLastName" onChange={this.handleChange} required />
+                            </div>
+                            <div className="form-group">
+                                <label>People code</label>
+                                <input type="text" className="form-control" name="newPeopleCode" onChange={this.handleChange} />
+                            </div>
+                            <div className="form-group">
+                                <label >Age group</label>
+                                <select className="form-control" name="newPeopleAgeGroup" onChange={this.handleChange} required>
+                                    <option value="">Select age group</option>
+                                    <option value="01-05">01-05</option>
+                                    <option value="06-12">06-12</option>
+                                    <option value="13-19">13-19</option>
+                                    <option value="20-40">20-40</option>
+                                    <option value="41-50">41-50</option>
+                                    <option value="51-60">51-60</option>
+                                    <option value="60-Above">60-Above</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label >Gender</label>
+                                <select className="form-control" name="newPeopleGender" onChange={this.handleChange} required>
+                                    <option value="">Select Gender</option>
+                                    <option value="Female">Female</option>
+                                    <option value="Male">Male</option>
+                                    <option value="Other">Other</option>
+                                </select>
+                            </div>
+                            <div className="form-group">
+                                <label>Notes</label>
+                                <textarea rows="4" className="form-control" name="newPeopleNotes" onChange={this.handleChange} />
+                            </div>
+                            <button type="submit" className="btn btn-success" >Submit</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        );
+    }
+});
+module.exports = AddPeople;
