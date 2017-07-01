cdd1024914b76f952046a23c2e7e918e86a8ea40
Erin <eringlabe@gmail.com>
2017-07-01 16:03:42 -0700

    Contact us component update

--- app/components/home/ContactUs.js
+++ app/components/home/ContactUs.js
@@ -34,20 +34,18 @@ var ContactUs = React.createClass({
             <div className="row" id="contact-us">
                 <NotificationSystem ref="notificationSystem" />
                 <div className="col-md-6 col-md-offset-3">
+                    <h3>Contact Giving Good(s)</h3>
                     <div className=" panel panel-default">
                         <div className="panel-body">
                             <form onSubmit={this.handleSubmit}>
                                 <div className="form-group">
-                                    <label htmlFor="clientEmail">Email</label>
-                                    <input type="text" placeholder="Enter your email address" className="form-control" id="clientEmail" onChange={this.handleChange} required />
+                                    <input type="text" placeholder="Your Email Address" className="form-control" id="clientEmail" onChange={this.handleChange} required />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="clientSubject">Subject</label>
-                                    <input type="text" placeholder="Enter Subject" className="form-control" id="clientSubject" onChange={this.handleChange} required />
+                                    <input type="text" placeholder="Subject" className="form-control" id="clientSubject" onChange={this.handleChange} required />
                                 </div>
                                 <div className="form-group">
-                                    <label htmlFor="clientMessage">Message</label>
-                                    <textarea placeholder="Write your message here" className="form-control" id="clientMessage" onChange={this.handleChange} rows="5" required />
+                                    <textarea placeholder="Your Message" className="form-control" id="clientMessage" onChange={this.handleChange} rows="5" required />
                                 </div>
                                 <button type="submit" className="btn btn-primary">Send</button>
 
