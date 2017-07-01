75e537eec5c1f1e1c80935b5fd93a2b8d985fa95
radhika sivarajan <radhikaraj333@gmail.com>
2017-07-01 13:01:57 -0700

    Links in button

--- app/components/home/Banner.js
+++ app/components/home/Banner.js
@@ -1,11 +1,13 @@
 var React = require("react");
+var Link = require("react-router").Link;
+
 var Banner = React.createClass({
     render: function () {
         return (
             <div className="row" id="banner">
                 <div id="btnDiv" className="">
-                    <button type="button" className="bannerBTN btn btn-secondary btn-lg">Donate</button>
-                    <button type="button" className="bannerBTN btn btn-secondary btn-lg">Shelter</button>
+                    <button type="button" className="bannerBTN btn btn-secondary btn-lg"><Link to="/donate"> Donate goods</Link></button>
+                    <button type="button" className="bannerBTN btn btn-secondary btn-lg"><Link to="/shelter"> Shelter</Link></button>
                 </div>
             </div>
         );
