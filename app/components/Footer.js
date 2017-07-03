615d192f1deb5d0903167c271ff605bde5c6daeb
Yousrat <ybanna@gmail.com>
2017-07-03 12:03:43 -0700

    linking image

--- app/components/Footer.js
+++ app/components/Footer.js
@@ -1,14 +1,12 @@
 var React = require("react");
-var Link = require("react-router").Link;
-
 var Footer = React.createClass({
     render: function () {
         return (
             <div className="row text-center footer">
                 <p>Copyright &copy; 2017 REMY</p>
-                <Link to="https://github.com/Yousrat/giving-goods">
+                <a href="https://github.com/Yousrat/giving-goods">
                     <img id="github" src="/assets/images/Github.png" alt="Github Code" />
-                </Link>
+                </a>
 
             </div>
         );
