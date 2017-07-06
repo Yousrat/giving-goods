d0448c93dc1d98ea8b54545e25b35296a52f7a3d
Erin <eringlabe@gmail.com>
2017-07-05 17:42:09 -0700

    Resolved merge conflict

--- app/components/donate/ListPeople.js
+++ app/components/donate/ListPeople.js
@@ -137,16 +137,6 @@ var ListPeople = React.createClass({
             return this.state.allPeopleList.map((person, index) => {
                 if (person.items.length) {
                     return (
-<<<<<<< HEAD
-                        <div className="people-block" key={index}>
-                            <p className="people-name capitalize-name">{person.person_first_name}</p>
-                            <p>{person.shelter_id.location}</p>
-                            <p>Age: {person.age_group}</p>
-                            <p>{person.gender}</p>
-                            <p>{person.notes}</p>
-                            <button type="button" className="btn btn-default btn-sm" data-toggle="modal" data-target="#personModal" onClick={this.setPersonItems.bind(this, person)}>More</button>
-                            <button type="button" className="btn btn-default btn-sm" data-toggle="modal" data-target="#contactModal" onClick={this.contactShelter.bind(this, person.shelter_id)}>Contact Shelter</button>
-=======
                         <div className="col-md-3 col-sm-4" key={index}>
                             <div className="people-block clearfix">
                                 <p className="people-name capitalize-name">{person.person_first_name}</p>
@@ -157,7 +147,6 @@ var ListPeople = React.createClass({
                                 <button type="button" className="btn btn-default btn-sm" data-toggle="modal" data-target="#personModal" onClick={this.setPersonItems.bind(this, person)}>More</button>
                                 <button type="button" className="btn btn-default btn-sm" data-toggle="modal" data-target="#contactModal" onClick={this.contactShelter.bind(this, person.shelter_id)}>Contact Shelter</button>
                             </div>
->>>>>>> 2f89ccae8370ad9bc15c0589d3f1d1eea49981ce
                         </div>
                     );
                 }
